1f717c88245ade99a64164eebb8ef46f906a93b0
david prada linarez <davidprali27@gmail.com>
2020-07-17 17:45:30 -0500

    api-login

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require("config");
 const auth = require("./components/auth/authRoutes");
 const user = require("./components/user/userRoutes");
 
@@ -10,5 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 //Routes
 app.use("/api/user", user);
 app.use("/api/auth", auth);
+
+app.get("/api", (req, res) => {
+  res.json({
+    USER: `http://localhost:${config.get("configDB.PORT")}/api/user`,
+    AUTH: `http://localhost:${config.get("configDB.PORT")}/api/auth`,
+  });
+});
 
 module.exports = app;
